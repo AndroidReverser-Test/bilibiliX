03cfba0169b7d8b6a9a385c7a288e1cfbe64b97b
feilongzaitia <A2322146826@163.com>
2024-01-18 14:56:26 +0800

    commit

--- app/src/main/java/com/bili/biliquguanggao/MainActivity.java
+++ app/src/main/java/com/bili/biliquguanggao/MainActivity.java
@@ -2,17 +2,12 @@ package com.bili.biliquguanggao;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View mytext = findViewById(R.id.sample_text);
-        mytext.setVisibility(View.GONE);
-
     }
 
 }
